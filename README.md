35796ea0ec9a999593608d729a59bbc6ac9f742d
unknown <isrohutama@yahoo.com>
2021-10-31 10:20:18 +0800

    Change dcf input to use yaml

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # fundamental_analysis
 
 This repo contains:
-1. Fundamental analysis script. <br/> This script computes and displays financial information of a company over time from its financial statements that are extracted from Macrotrends. It also computes some share price ratios. The price data is taken from yahoo finance.  
-2. DCF (Discounted Cash Flow) calculator. <br/> 
+1. Fundamental analysis script.<br/>This script computes and displays financial information of a company over time from its financial statements that are extracted from Macrotrends. It also computes some share price ratios. The price data is taken from yahoo finance.  
+2. DCF (Discounted Cash Flow) calculator. 
 
 <img src="output_example.gif" alt="output_example" width="1024"/>
 
@@ -10,12 +10,13 @@ This repo contains:
 - Python 3.8 and other packages in anaconda 2020.11
 - yfinance 0.1.63
 
-## How to use fundamental anaysis code
+## How to use fundamental anaysis script
 1. Open "fa_run.py" and modify all required information under "REQUIRED USER INPUTS".
-2. Run "fa_run.py".
+2. Run "python fa_run.py".
 3. The result will be inside a folder named "output/..." where "..." is "output_prefix" in step 1 and followed by current date-time.
 4. Open "0_fundamental_analysis.md" with browser (tested on firefox).
 
 ## How to use DCF calculator
-1. Open "dcf.py" and modify all required information under "REQUIRED USER INPUTS".
-2. Run "dcf.py".
+1. Open "dcf_input_yaml.py".
+2. Search the company name and update its information if it is needed.<br/> If the company name is not in the list, create a new entry and fill all required information (You can use other company entry available as a template).
+2. Run "python dcf.py <the_company_name>". It must be launched from the same directory as "dcf.py". 
--- dcf.py
+++ dcf.py
@@ -1,93 +1,97 @@
 '''
 20 years discounted cash flow.
 It is developed by watching DCF computation videos by Adam Khoo.
-In his videos, the CF used is the operating CF, but 
-the free CF can be used instead to be more conservative, or Net income.
+Here the CF used is OCF, FCF, net income.
 '''
 
-##########################################
-########## REQUIRED USER INPUTS ##########
-##########################################
-'''
-discount rate = risk free rate + beta * market risk premium
-risk free rate --> usually from 10 year treasury rate
-
-US stocks (from Adam Khoo's video)
-risk free rate = 0.64%
-average market risk premium = 5%
-    Beta    Discount Rate
-    <0.9    4.6%
-    1       5.6%
-    1.1     6.1%
-    1.2     6.6%
-    1.3     7.1%
-    1.4     7.6%
-    1.5     8.1%
-    >1.6    8.6%
-
-China/HK stocks (from Adam Khoo's video)
-risk free rate = 0.6%
-average market risk premium = 6.6%
-    Beta    Discount Rate
-    <0.9    5.9%
-    1       7%
-    1.1     7.9%
-    1.2     9%
-    1.3     9.2%
-    1.4     10%
-    1.5     10.5%
-    >1.6    11%
-''' 
-
-## Amazon 20211029 --> undervalued (4231 - Net)
-cf = 29438  # in millions (FCF=7067, OCF=59323, Net=29438)
-debt = 106576  # short + long term debt, in millions
-cash = 89894  # cash and short term investment, in millions
-growth_y1_y5 = 37.13  # in percent (from yfinance)
-growth_y6_y10 = 18.5  # in percent
-growth_y11_y20 = 1.72  # in percent (ave US GDP growth in trailing 20y = 1.72)
-shr_out = 506  # share outstanding, in millions
-dr = 7.71  # discount rate, in percent (beta=1.16, risk free rate= 1.33%, market risk premium = 5.5%)
-
-##########################################
-############ PROCESSING CODE #############
-##########################################
-
+import argparse
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 
-div = 1. + dr/100.
-growth_y1_y5 = 1 + growth_y1_y5 / 100.
-growth_y6_y10 = 1 + growth_y6_y10 / 100.
-growth_y11_y20 = 1 + growth_y11_y20 / 100.
+## Retrieve company name from user input
+parser = argparse.ArgumentParser()
+parser.add_argument('company_name', type=str, help='Company name as in dcf_input_list.yaml')
+args = parser.parse_args()
 
-cf_pred = np.zeros(20)
-pv_cf_pred = np.zeros(20)
-for y in range(1, 5+1):
-    idx = y - 1
-    cf_pred[idx] = (cf * growth_y1_y5**y)
-    pv_cf_pred[idx] = cf_pred[idx] / div**y
-for y in range(6, 10+1):
-    idx = y - 1
-    cf_pred[idx] = (cf_pred[4] * growth_y6_y10**(y - 5))
-    pv_cf_pred[idx] = cf_pred[idx] / div**y
-for y in range(11, 20+1):
-    idx = y - 1
-    cf_pred[idx] = (cf_pred[9] * growth_y11_y20**(y - 10))
-    pv_cf_pred[idx] = cf_pred[idx] / div**y
+## Retrieve required input data from dcf_input_list.yaml
+with open('dcf_input_list.yaml', 'r') as f:
+    data = yaml.safe_load(f)
+if 'fcf' in data[args.company_name]:
+    fcf = data[args.company_name]['fcf']
+if 'ocf' in data[args.company_name]:
+    ocf = data[args.company_name]['ocf']
+if 'net' in data[args.company_name]:
+    net = data[args.company_name]['net']
+date = data[args.company_name]['date']
+debt = data[args.company_name]['debt']
+cash = data[args.company_name]['cash']
+growth_y1_y5 = 1 + data[args.company_name]['growth_y1_y5'] / 100.
+growth_y6_y10 = 1 + data[args.company_name]['growth_y6_y10'] / 100.
+growth_y11_y20 = 1 + data[args.company_name]['growth_y11_y20'] / 100.
+shr_out = data[args.company_name]['shr_out']
+dr = data[args.company_name]['dr']
 
-pv_20y = np.sum(pv_cf_pred)
-int_val_per_share = (pv_20y - debt + cash) / shr_out
-print('Intrinsic value per share: ' + str(int_val_per_share))
+## DCF function
+def dcf_func(cf):
+    div = 1. + dr/100.
+    
+    cf_pred = np.zeros(20)
+    pv_cf_pred = np.zeros(20)
+    for y in range(1, 5+1):
+        idx = y - 1
+        cf_pred[idx] = (cf * growth_y1_y5**y)
+        pv_cf_pred[idx] = cf_pred[idx] / div**y
+    for y in range(6, 10+1):
+        idx = y - 1
+        cf_pred[idx] = (cf_pred[4] * growth_y6_y10**(y - 5))
+        pv_cf_pred[idx] = cf_pred[idx] / div**y
+    for y in range(11, 20+1):
+        idx = y - 1
+        cf_pred[idx] = (cf_pred[9] * growth_y11_y20**(y - 10))
+        pv_cf_pred[idx] = cf_pred[idx] / div**y
 
-fig = plt.figure()
-ax = plt.axes()
-ax.plot(np.arange(1, 21), cf_pred, label='projected CF')
-ax.plot(np.arange(1, 21), pv_cf_pred, label='discounted CF')
-ax.set_xlabel('Year')
-ax.set_ylabel('Million USD')
-ax.set_title('PV of CF')
-ax.grid()
-ax.legend()
+    pv_20y = np.sum(pv_cf_pred)
+    int_val_per_share = (pv_20y - debt + cash) / shr_out
+
+    return int_val_per_share, cf_pred, pv_cf_pred
+
+print('Company: ' + args.company_name + ' (' + date + ')')
+if 'fcf' in data[args.company_name]:
+    int_val_per_share, cf_pred, pv_cf_pred = dcf_func(fcf)
+    print('Intrinsic value per share (FCF): ' + str(int_val_per_share))
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(np.arange(1, 21), cf_pred, label='projected CF')
+    ax.plot(np.arange(1, 21), pv_cf_pred, label='discounted CF')
+    ax.set_title('Based on FCF')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Million USD')
+    ax.grid()
+    ax.legend()
+if 'ocf' in data[args.company_name]:
+    int_val_per_share, cf_pred, pv_cf_pred = dcf_func(ocf)
+    print('Intrinsic value per share (OCF): ' + str(int_val_per_share))
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(np.arange(1, 21), cf_pred, label='projected CF')
+    ax.plot(np.arange(1, 21), pv_cf_pred, label='discounted CF')
+    ax.set_title('Based on OCF')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Million USD')
+    ax.grid()
+    ax.legend()
+if 'net' in data[args.company_name]:
+    int_val_per_share, cf_pred, pv_cf_pred = dcf_func(net)
+    print('Intrinsic value per share (Net): ' + str(int_val_per_share))
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(np.arange(1, 21), cf_pred, label='projected CF')
+    ax.plot(np.arange(1, 21), pv_cf_pred, label='discounted CF')
+    ax.set_title('Based on Net')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Million USD')
+    ax.grid()
+    ax.legend()
 
 plt.show()
